d9d5ccfb06c56ebf9e09deefc63a341daaa46d32
rezaruslan94 <reza.ruslan94@gmail.com>
2017-01-03 09:57:48 +0700

    show total price in order's index page

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,12 @@ class Order < ApplicationRecord
 
   accepts_nested_attributes_for :order_items, allow_destroy: true
 
+  def calculate_total_price
+    sum = 0
+    self.order_items.each do |order_item|
+      sum = sum + (order_item.qty * order_item.product.price)
+    end
+    self.update(total_price: sum)
+  end
+
 end
